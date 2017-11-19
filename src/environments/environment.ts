2d2f157c032843b769b56c7e75ecd35dacb259a9
Mani <im.manikanta@gmail.com>
2017-11-19 22:02:18 +0530

    env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "",
+    authDomain: "anonymous-97084.firebaseapp.com",
+    databaseURL: "https://anonymous-97084.firebaseio.com",
+    projectId: "anonymous-97084",
+    storageBucket: "anonymous-97084.appspot.com",
+    messagingSenderId: ""
+  }
+};
